1426f16c6e3cfef2729c4a1c96ca6d8e22900f7d
Phung Hương <Kirigayakazuto07103009@gmail.com>
2026-07-02 10:21:57 +0700

    fix config

--- QuanLyKTX_user/config.php
+++ QuanLyKTX_user/config.php
@@ -1,6 +1,12 @@
 <?php
 // config.php
-define('BASE_URL', 'http://localhost:8080/QuanLyKTX_user/'); // Điều chỉnh lại cho khớp với đường dẫn thực tế của bạn
+
+// Thêm điều kiện kiểm tra tồn tại cho BASE_URL
+if (!defined('BASE_URL')) {
+    define('BASE_URL', 'http://localhost:8080/QuanLyKTX_user/');
+}
+
+// Giữ nguyên phần API_URL đã có
 if (!defined('API_URL')) {
     define('API_URL', 'http://localhost:8080/QuanLyKTX_API/Routes/apiUser.php');
 }
