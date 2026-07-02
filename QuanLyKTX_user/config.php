eec50999f6663c0246f615609e53d33766a23424
Phung Hương <Kirigayakazuto07103009@gmail.com>
2026-07-02 10:25:16 +0700

    xoa base_url trong config

--- QuanLyKTX_user/config.php
+++ QuanLyKTX_user/config.php
@@ -1,10 +1,6 @@
 <?php
 // config.php
 
-// Thêm điều kiện kiểm tra tồn tại cho BASE_URL
-if (!defined('BASE_URL')) {
-    define('BASE_URL', 'http://localhost:8080/QuanLyKTX_user/');
-}
 
 // Giữ nguyên phần API_URL đã có
 if (!defined('API_URL')) {
